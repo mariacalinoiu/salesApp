90f4872100ab67179e15247a02f90ee186b75005
maria.calinoiu <maria.calinoiu@emag.ro>
2021-01-24 21:51:05 +0200

    added access control to header

--- src/handlers/unitati_de_masura.go
+++ src/handlers/unitati_de_masura.go
@@ -38,6 +38,13 @@ func HandleUnitatiDeMasura(w http.ResponseWriter, r *http.Request, db datasource
 		return
 	}
 
+	if origin := r.Header.Get("Origin"); origin != "" {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization,X-CSRF-Token")
+		w.Header().Set("Access-Control-Expose-Headers", "Authorization")
+	}
+
 	status = http.StatusOK
 	logger.Printf("Status: %d %s", status, http.StatusText(status))
 }
